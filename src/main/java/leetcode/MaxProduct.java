e06fa620d75e699d3365d80ae1f986f813c11f34
sujy <sujy@akulaku.com>
2021-02-09 17:42:24 +0800

    clean up to the code

--- src/main/java/leetcode/MaxProduct.java
+++ src/main/java/leetcode/MaxProduct.java
@@ -8,14 +8,14 @@ public class MaxProduct {
     public static int maximumProduct(int[] nums) {
 
         QuickSort(nums, 0, nums.length - 1);
-        if (nums.length >= 3){
-            return nums[nums.length - 1] *  nums[nums.length -2] * nums[nums.length - 3] ;
+        if (nums.length >= 3) {
+            return nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        int[] ints = {-100,-98,-1,2,3,4};
+        int[] ints = {-100, -98, -1, 2, 3, 4};
         int i = maximumProduct(ints);
         System.out.println(i);
 
--- src/main/java/leetcode/Palindrome.java
+++ src/main/java/leetcode/Palindrome.java
@@ -4,24 +4,25 @@ public class Palindrome {
     public static void main(String[] args) {
         boolean palindrome = isPalindrome(0);
         System.out.println(palindrome);
-        System.out.println(-7%-3);
+        System.out.println(-7 % -3);
     }
 
     /**
      * 判断是不是回文数
+     *
      * @param number
      * @return
      */
-    public static boolean  isPalindrome(int number){
-        if(number < 0) return false;
-        if(number == 0) return true;
+    public static boolean isPalindrome(int number) {
+        if (number < 0) return false;
+        if (number == 0) return true;
         int palindrome = 0;
         int expect = number;
-        while (number != 0){
+        while (number != 0) {
             palindrome = palindrome * 10 + number % 10;
             number /= 10;
         }
-        return  palindrome == expect;
+        return palindrome == expect;
 
     }
 
