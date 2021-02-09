79320539143d593cacd3101a268cd9d61af7d07a
sujy <sujy@akulaku.com>
2021-02-09 17:45:47 +0800

    clean up to the code

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -5,19 +5,19 @@ public class QuickSort {
      * @param args
      */
     public static void main(String[] args) {
-        int[] array = {10, 20, 15 , 16, 18 , 17, 12, 13, 19, 11, 14};
+        int[] array = {10, 20, 15, 16, 18, 17, 12, 13, 19, 11, 14};
         QuickSort(array, 0, array.length - 1);
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
 
     }
 
     public static void QuickSort(int[] array, int low, int high) {
-        if(low < high){
+        if (low < high) {
             int position = Partition(array, low, high);
-            QuickSort(array, low, position-1);
-            QuickSort(array, position+1, high);
+            QuickSort(array, low, position - 1);
+            QuickSort(array, position + 1, high);
         }
     }
 
@@ -26,10 +26,10 @@ public class QuickSort {
         int end = high + 1;
         int compare = array[low];
         int tmp;
-        while(true){
-            while(array[++start] < compare && start < high);
-            while(array[--end] > compare);
-            if(start >= end) break;
+        while (true) {
+            while (array[++start] < compare && start < high) ;
+            while (array[--end] > compare) ;
+            if (start >= end) break;
             tmp = array[start];
             array[start] = array[end];
             array[end] = tmp;
