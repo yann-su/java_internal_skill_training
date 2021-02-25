d155855caa6cfe81439abffa3d3532962ae6c586
sujy <sujy@akulaku.com>
2021-02-25 18:49:49 +0800

    add double list

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -21,13 +21,41 @@ public class TwoSum {
     }
 
 
+    /**
+     * 两个链表相加,问题出在链表赋值问题，思路没问题，但是面试时候看的是对错，记住这点
+     * 
+     * @param l1
+     * @param l2
+     * @return
+     */
+    // TODO: 2021/2/25 学习链表赋值 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        while (l1.next != null && l2.next != null){
-
+        ListNode head = null,tail = null;
+        int divisor = 0;
+        while (l1 != null || l2 != null){
+            int l1data = l1 != null ? l1.val : 0;
+            int l2data = l2 != null ? l2.val : 0;
+            int data =  l1data + l2data + divisor;
+            if (head == null){
+                head = tail =  new ListNode(data % 10,null);
+            }
+            else{
+                tail.next = new ListNode(data % 10,null);
+                tail = tail.next;
+            }
+            divisor =  data / 10;
+            if ( l1 != null){
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                l2 = l2.next;
+            }
         }
-        return null;
+        if (divisor > 0){
+            tail.next = new ListNode(divisor,null);
+        }
+        return head;
     }
 
 
@@ -50,7 +78,10 @@ public class TwoSum {
         int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
         System.out.println(ints[0]);
         System.out.println(ints[1]);
-        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode head = new ListNode(9, new ListNode(9, new ListNode(9, null)));
+        ListNode head1 = new ListNode(9, new ListNode(9, null));
+        ListNode listNode = addTwoNumbers(head, head1);
+        System.out.println(listNode);
 
 
     }
