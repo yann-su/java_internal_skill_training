69a4f8d50f0ac2bc113db3079bb03a67ae82a6b9
sujy <sujy@akulaku.com>
2021-03-30 18:14:51 +0800

    add flink1.12 for read hive

--- flink_run/src/main/java/task/HiveWordCount.java
+++ flink_run/src/main/java/task/HiveWordCount.java
@@ -15,15 +15,17 @@ public class HiveWordCount {
 //        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().build();
         TableEnvironment tableEnv = TableEnvironment.create(settings);
-        String name            = "default";
-        String defaultDatabase = "default";
+        String name            = "feat";
+        String defaultDatabase = "feat";
         String hiveConfDir     = "/Users/backbook/opt/hive-conf";
         HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir);
 
-        tableEnv.registerCatalog("default",hive);
-        tableEnv.useCatalog("default");
-        String sql = "select * from default.relation_multi_afi_afiuid_phonenumber_openpay";
+        tableEnv.registerCatalog("feat",hive);
+        tableEnv.useCatalog("feat");
+//        String sql = "insert into feat.relation_multi_afi_afiuid_phonenumber_openpay select" +
+//                " cast(19 as BIGINT),CAST('21311' as STRING),CAST(3123131 as BIGINT),CAST(31231 as BIGINT),CAST(3213141 AS BIGINT),CAST(2013 AS INT) ";
 
+        String sql = "select * from tableA";
         tableEnv.executeSql(sql).print();
 
 
