ce61c79b5ee59d5f94f4be5d2b5696a940d6a5e9
sujy <sujy@akulaku.com>
2021-04-23 17:09:14 +0800

    add Callable and Executor

--- concurrent_programming/src/main/java/executors/CallableTest.java
+++ concurrent_programming/src/main/java/executors/CallableTest.java
@@ -1,18 +1,33 @@
 package executors;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class CallableTest implements Callable<String> {
     @Override
     public String call() throws Exception {
         try {
 
-
+            String a = "return String";
+            return a;
         }catch (Exception e){
             e.printStackTrace();
             return "exception";
         }
+    }
 
-        return null;
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
+        Future<String> feature = threadPoolExecutor.submit(new CallableTest());
+
+        try {
+            System.out.println(feature.get());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            threadPoolExecutor.shutdown();
+        }
+
+
     }
 }
