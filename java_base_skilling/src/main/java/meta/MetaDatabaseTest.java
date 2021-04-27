40ab815681582261520606fdcd93bee02e53b06e
sujy <sujy@akulaku.com>
2021-04-27 11:21:01 +0800

    add docker-compose.yml and MetaDatabaseTest

---
+++ java_base_skilling/src/main/java/meta/MetaDatabaseTest.java
@@ -0,0 +1,42 @@
+package meta;
+
+import com.github.abel533.database.*;
+import com.github.abel533.utils.DBMetadataUtils;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MetaDatabaseTest {
+
+    public static void main(String[] args) {
+        SimpleDataSource dataSource = new SimpleDataSource(
+                Dialect.MYSQL,
+                "jdbc:mysql://42.193.142.13:3306/",
+                "root",
+                "@!l8TD1Ew5r3"
+        );
+        DBMetadataUtils dbMetadataUtils = null;
+        try {
+            dbMetadataUtils = new DBMetadataUtils(dataSource);
+
+            DatabaseConfig defaultConfig = dbMetadataUtils.getDefaultConfig();
+            List<IntrospectedTable> list = dbMetadataUtils.introspectTables(new DatabaseConfig("wordpress",null,"wp_comments"));
+//            List<IntrospectedTable> list = dbMetadataUtils.introspectTables(defaultConfig);
+
+            for (IntrospectedTable table : list) {
+                System.out.println(table.getName() + ":");
+                for (IntrospectedColumn column : table.getAllColumns()) {
+                    System.out.println(column.getName() + " - " +
+                            column.getJdbcTypeName() + " - " +
+                            column.getJavaProperty() + " - " +
+                            column.getJavaProperty() + " - " +
+                            column.getFullyQualifiedJavaType().getFullyQualifiedName() + " - " +
+                            column.getRemarks());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
