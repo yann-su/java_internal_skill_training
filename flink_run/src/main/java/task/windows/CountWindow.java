09c5c27bb54c3001696469b8d2c11ac5618f6045
sujy <sujy@akulaku.com>
2021-05-12 17:23:29 +0800

    rename CountWindow

--- flink_run/src/main/java/task/windows/CountWindow.java
+++ flink_run/src/main/java/task/windows/CountWindow.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 
-public class SocketCountWindow {
+public class CountWindow {
 
     public static void main(String[] args) throws Exception {
 
